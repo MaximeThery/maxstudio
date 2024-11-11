d2b2f18632102575925110a1cf8fe22e42cb058d
maxime.thery <ythery13@gmail.com>
2024-11-11 19:14:24 +0100

    travaux sur le header

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function charger_styles() {
     wp_enqueue_style('accueil', get_stylesheet_directory_uri() . '/css/accueil.css');
     wp_enqueue_style('tout', get_stylesheet_directory_uri() . '/css/all.css');
+    wp_enqueue_style('header', get_stylesheet_directory_uri() . '/css/header.css');
 }
 add_action('wp_enqueue_scripts', 'charger_styles');
 
